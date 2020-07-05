85e44ee8192df15d06649707645a7bdb7e9c268b
Dharmesh Patel <dpatel554@gmail.com>
2020-07-05 14:46:43 -0500

    initial commit

---
+++ Project1/src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Yes");
+       
+       Test test = new Test();
+  	    test.tacos();
+	}
+	
+	
+	public void tacos() {
+		// TODO Auto-generated method stub
+       System.out.println("you are hungry");
+       
+	}
+
+}
