af3f448a71cc6d0377af88f715f82552403ec50c
Dharmesh Patel <dpatel554@gmail.com>
2020-07-06 23:08:27 -0500

    Scanner Code

--- Project1/src/Test.java
+++ Project1/src/Test.java
@@ -1,13 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-       System.out.println("Yes");
        
-       Test test = new Test();
-  	    test.tacos();
+		Scanner scanner = new Scanner(new File("src/Names.txt"));
+		List<String> lastName = new ArrayList<String>();
+		
+		
+		while(scanner.hasNext()) {
+			lastName.add(scanner.nextLine());
+			
+		}
+		
+		for (int i=0; i < lastName.size(); i++) {
+			System.out.println("Your LastName is: " +lastName.get(i));
+			
+		}
+		
+		 scanner.close();
+		
 	}
+	    
+	
 	
 	
 	public void tacos() {
